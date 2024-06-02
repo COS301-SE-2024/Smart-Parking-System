caac627095832b5fdc20f1449a32b55f662bf04a
Mr.Who <106270348+MrWhoCN@users.noreply.github.com>
2024-06-02 16:09:45 +0200

    fixed the style

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-// import 'package:flutter/material.dart';
-// import 'components/login/login.dart';
-// import 'components/cancelBooking/cancelBooking.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Signup & Login',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: LoginPage(),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'components/login/login.dart';
+import 'components/bookings/cancelBooking.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup & Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
 
 
 
