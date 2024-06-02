437676cdaf135cac16ed1e73056080086259977a
Mr.Who <106270348+MrWhoCN@users.noreply.github.com>
2024-06-02 16:30:30 +0200

    fixed the bug

--- lib/components/bookings/cancelBooking.dart
+++ lib/components/bookings/cancelBooking.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Smart Parking System',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CancelBookingPage(),
-    );
-  }
-}
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Smart Parking System',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: CancelBookingPage(),
+//     );
+//   }
+// }
 
 class CancelBookingPage extends StatefulWidget {
   @override
