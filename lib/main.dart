8b0471ce6a1eddd60963ba975b03fcb9ff733201
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-06-19 18:51:30 +0200

    remove tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_parking_system/components/login/login_main.dart';
-import 'components/login/login.dart';
+//import 'components/login/login.dart';
 
 void main() {
   runApp(const MyApp());
