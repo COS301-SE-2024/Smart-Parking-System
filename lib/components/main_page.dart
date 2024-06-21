cd8554eb6dc87ecfad683a7b55615c492116cb5d
Shiza <u21451631@tuks.co.za>
2024-06-21 13:11:00 +0200

    updated mainpage

--- lib/components/main_page.dart
+++ lib/components/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainPage extends StatelessWidget {
   @override
@@ -9,45 +8,47 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.black),
-          onPressed: () {
-            // Handle menu action
-          },
+        leading: Container(
+          margin: EdgeInsets.only(top: 20.0), // Adjust this value to move the icon down
+          child: IconButton(
+            icon: Icon(Icons.menu, color: Colors.black, size: 30.0), // Increase the size of the menu icon
+            onPressed: () {
+              // Handle menu action
+            },
+          ),
         ),
       ),
       body: Stack(
         children: <Widget>[
-          GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: LatLng(51.5074, -0.1278),
-              zoom: 13.0,
-            ),
-            markers: {
-              Marker(
-                markerId: MarkerId('1'),
-                position: LatLng(51.5074, -0.1278),
-                infoWindow: InfoWindow(title: 'Marker 1'),
+          // Background image of a map
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('../assets/map.webp'), // Update the path to your background image
+                fit: BoxFit.cover,
               ),
-            },
+            ),
           ),
           Positioned(
-            top: 50.0,
+            top: 80.0, // Move the box down
             left: 20.0,
             right: 20.0,
             child: Container(
-              padding: EdgeInsets.all(10.0),
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              height: 50.0, // Adjust the height to make the box smaller
               decoration: BoxDecoration(
                 color: Color(0xFF35344A),
                 borderRadius: BorderRadius.circular(10.0),
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Where are you going?',
-                  border: InputBorder.none,
-                  hintStyle: TextStyle(color: Colors.white),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Where are you going?',
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  style: TextStyle(color: Colors.white),
                 ),
-                style: TextStyle(color: Colors.white),
               ),
             ),
           ),
@@ -103,7 +104,7 @@ class MainPage extends StatelessWidget {
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       image: DecorationImage(
-                        image: AssetImage('../assets/chart.png'),
+                        image: AssetImage('../assets/history.png'),
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -124,7 +125,7 @@ class MainPage extends StatelessWidget {
             ),
           ),
           Positioned(
-            bottom: 55,
+            bottom: 40,
             left: MediaQuery.of(context).size.width / 2 - 28,
             child: GestureDetector(
               onTap: () {
