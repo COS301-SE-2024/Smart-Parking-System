813401fbb1dd6ad5fa877ae23a95af13e7603e36
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-06-23 13:52:00 +0200

    Settings Page Added

---
+++ lib/components/settings/settings.dart
@@ -0,0 +1,243 @@
+import 'package:flutter/material.dart';
+import 'package:smart_parking_system/components/main_page.dart';
+import 'package:smart_parking_system/components/payment/payment_options.dart';
+import 'package:smart_parking_system/components/profile/userprofile.dart';
+import 'package:smart_parking_system/components/sidebar.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  int _selectedIndex = 3;
+  bool _isSwitched = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF35344A),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 30),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              color: const Color(0xFF35344A),
+              child: Stack(
+                children: [
+                  Builder(
+                    builder: (BuildContext context) {
+                      return IconButton(
+                        icon: const Icon(Icons.menu, color: Colors.white, size: 30.0),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer(); // Open the drawer
+                        },
+                      );
+                    },
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Settings',
+                      style: TextStyle(
+                        color: Colors.tealAccent,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.grey,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'John Doe',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+            const Text(
+              'Account Settings',
+              style: TextStyle(
+                color: Color(0xFFADADAD),
+                fontSize: 16,
+              ),
+            ),
+            ListTile(
+              title: const Text('Edit profile', style: TextStyle(color: Colors.white)),
+              trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white, size: 20),
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (_) => const UserProfilePage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Add a payment method', style: TextStyle(color: Colors.white)),
+              trailing: const Icon(Icons.add, color: Colors.white, size: 20),
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (_) => const PaymentMethodPage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Push notifications', style: TextStyle(color: Colors.white)),
+              trailing: Switch(
+                value: _isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    _isSwitched = value;
+                  });
+                },
+                activeColor: Colors.tealAccent,
+              ),
+            ),
+            const Divider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+            const Text(
+              'More',
+              style: TextStyle(
+                color: Color(0xFFADADAD),
+                fontSize: 16,
+              ),
+            ),
+            const ListTile(
+              title: Text('About us', style: TextStyle(color: Colors.white)),
+              trailing: Icon(Icons.arrow_forward_ios, color: Colors.white, size: 20),
+            ),
+            const ListTile(
+              title: Text('Privacy policy', style: TextStyle(color: Colors.white)),
+              trailing: Icon(Icons.arrow_forward_ios, color: Colors.white, size: 20),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: const Color(0xFF35344A),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            // color: const Color(0xFF2C2C54),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.6),
+                spreadRadius: 1,
+                blurRadius: 8,
+                offset: const Offset(0, -3),
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: const Color(0xFF35344A), // To ensure the Container color is visible
+            currentIndex: _selectedIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.wallet, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined, size: 30),
+                label: '',
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+
+                if (_selectedIndex == 0) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const MainPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 1) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const PaymentMethodPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 2) {
+                  // Add navigation if necessary
+                } else if (_selectedIndex == 3) {
+                  // Add navigation if necessary
+                }
+              });
+            },
+            selectedItemColor: const Color(0xFF58C6A9),
+            unselectedItemColor: Colors.grey,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFF58C6A9),
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.near_me,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      drawer: const SideMenu(),
+    );
+  }
+}
+
+class CustomCenterDockedFABLocation extends FloatingActionButtonLocation {
+  final double offset;
+
+  CustomCenterDockedFABLocation(this.offset);
+
+  @override
+  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+    // Position the FAB slightly higher than centerDocked
+    final double fabX = (scaffoldGeometry.scaffoldSize.width / 2) -
+        (scaffoldGeometry.floatingActionButtonSize.width / 2);
+    final double fabY = scaffoldGeometry.scaffoldSize.height -
+        scaffoldGeometry.bottomSheetSize.height -
+        scaffoldGeometry.snackBarSize.height -
+        (scaffoldGeometry.floatingActionButtonSize.height / 2) -
+        offset;
+    return Offset(fabX, fabY);
+  }
+}
--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -4,6 +4,7 @@ import 'package:smart_parking_system/components/login/login_main.dart';
 import 'package:smart_parking_system/components/notifications/notificationspage.dart';
 import 'package:smart_parking_system/components/payment/offers.dart';
 import 'package:smart_parking_system/components/payment/payment_options.dart';
+import 'package:smart_parking_system/components/settings/settings.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
@@ -107,7 +108,11 @@ class SideMenu extends StatelessWidget {
                     leading: const Icon(Icons.settings, color: Colors.white),
                     title: const Text('Settings', style: TextStyle(color: Colors.white)),
                     onTap: () {
-
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (_) => const SettingsPage(),
+                        ),
+                      );
                     },
                   ),
                 ],
