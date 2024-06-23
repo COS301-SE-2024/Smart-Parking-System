ee8481b9e03d56e710693ca6a119c51551c9e729
Mekhail Muller <64637963+MekhailMuller@users.noreply.github.com>
2024-06-23 20:54:24 +0200

    Fixed linting

--- lib/components/main_page.dart
+++ lib/components/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:smart_parking_system/components/bookings/make_booking.dart';
 import 'package:smart_parking_system/components/sidebar.dart';
 import 'package:smart_parking_system/components/payment/payment_options.dart';
 
@@ -109,11 +108,11 @@ class _MainPageState extends State<MainPage> {
               Center(
                 child: ElevatedButton(
                   onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (_) => const BookingPage(),
-                      ),
-                    );
+                    // Navigator.of(context).pushReplacement(
+                    //   MaterialPageRoute(
+                    //     builder: (_) => const BookingPage(),
+                    //   ),
+                    // );
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xFF58C6A9),
