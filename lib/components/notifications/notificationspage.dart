47038d5309af77b686a01a1d75c6f38717743184
Mekhail Muller <64637963+MekhailMuller@users.noreply.github.com>
2024-06-23 13:10:22 +0200

    Update notificationspage.dart

--- lib/components/notifications/notificationspage.dart
+++ lib/components/notifications/notificationspage.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 
-class NotificationApp extends StatelessWidget {
+import 'package:smart_parking_system/components/main_page.dart';
+import 'package:smart_parking_system/components/parking/parking_history.dart';
+import 'package:smart_parking_system/components/payment/payment_options.dart';
+import 'package:smart_parking_system/components/sidebar.dart';
+
+class NotificationApp extends StatefulWidget {
   const NotificationApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notification Page',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const NotificationPage(),
-    );
-  }
+  State<NotificationApp> createState() => _NotificationPageState();
 }
 
-class NotificationPage extends StatelessWidget {
-  const NotificationPage({super.key});
+class _NotificationPageState extends State<NotificationApp> {
+  int _selectedIndex = 0;
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,14 +28,15 @@ class NotificationPage extends StatelessWidget {
               color: const Color(0xFF2D2F41),
               child: Stack(
                 children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: IconButton(
-                      icon: const Icon(Icons.menu, color: Colors.white),
-                      onPressed: () {
-                        // Handle menu button
-                      },
-                    ),
+                  Builder(
+                    builder: (BuildContext context) {
+                      return IconButton(
+                        icon: const Icon(Icons.menu, color: Colors.white, size: 30.0),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer(); // Open the drawer
+                        },
+                      );
+                    },
                   ),
                   const Align(
                     alignment: Alignment.center,
@@ -93,6 +91,88 @@ class NotificationPage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: const Color(0xFF35344A),),
+        child: Container(
+        decoration: BoxDecoration(
+          // color: const Color(0xFF2C2C54),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.6),
+              spreadRadius: 1,
+              blurRadius: 8,
+              offset: const Offset(0, -3),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, 
+          backgroundColor: const Color(0xFF35344A), // To ensure the Container color is visible
+          currentIndex: _selectedIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined, size: 30),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wallet, size: 30),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history, size: 30),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined, size: 30),
+              label: '',
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+
+              if(_selectedIndex == 0){
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const MainPage(),
+                  ),
+                );
+              } else if(_selectedIndex == 1){
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const PaymentMethodPage(),
+                  ),
+                );
+              } else if(_selectedIndex == 2){
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const ParkingHistoryPage(),
+                  ),
+                );
+              } else if(_selectedIndex == 3){
+              
+              }
+            });
+          },
+          selectedItemColor: Colors.grey,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+        ),
+      ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //   backgroundColor: const Color(0xFF58C6A9),
+      //   shape: const CircleBorder(),
+      //   child: const Icon(
+      //     Icons.near_me,
+      //     color: Colors.white,
+      //   ), 
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      drawer: const SideMenu(),
     );
   }
 }
