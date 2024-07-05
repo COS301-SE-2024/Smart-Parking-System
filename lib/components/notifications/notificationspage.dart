a749b6b357ff030391a26d2a79c4de9fde8f1261
Mr.Who <106270348+MrWhoCN@users.noreply.github.com>
2024-07-05 15:42:55 +0200

    changed the text to variable and save the url link

--- lib/components/notifications/notificationspage.dart
+++ lib/components/notifications/notificationspage.dart
@@ -15,12 +15,23 @@ class NotificationApp extends StatefulWidget {
 class _NotificationPageState extends State<NotificationApp> {
   int _selectedIndex = 0;
 
+  //
+  final String appBarTitle = 'Notifications';
+  final String sectionTitleToday = 'Today';
+  final String sectionTitleYesterday = 'Yesterday';
+  final String sectionTitleThisWeek = 'This week';
+  final String notificationTime1 = '9:01am';
+  final String notificationTitle1 = 'Booked Successful';
+  final String notificationSubtitle1 = 'View Details';
+  final String notificationSubtitle2 = 'View Invoice';
+  final String notificationTitle2 = 'Alert';
+  final String notificationSubtitle3 = 'You have booked a Space at 9:10 am';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF2D2F41),
-      body: SingleChildScrollView( 
+      body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
@@ -38,11 +49,11 @@ class _NotificationPageState extends State<NotificationApp> {
                       );
                     },
                   ),
-                  const Align(
+                  Align(
                     alignment: Alignment.center,
                     child: Text(
-                      'Notifications',
-                      style: TextStyle(
+                      appBarTitle,
+                      style: const TextStyle(
                         color: Colors.tealAccent,
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
@@ -53,123 +64,114 @@ class _NotificationPageState extends State<NotificationApp> {
               ),
             ),
             Container(
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SectionTitle(title: 'Today'),
-                    NotificationCard(
-                      time: '9:01am',
-                      title: 'Booked Successful',
-                      subtitle: 'View Details',
-                      icon: Icons.check_circle,
-                      iconColor: Colors.tealAccent,
-                    ),
-                    SizedBox(height: 10),
-                    SectionTitle(title: 'Yesterday'),
-                    NotificationCard(
-                      time: '9:01am',
-                      title: 'Booked Successful',
-                      subtitle: 'View Invoice',
-                      icon: Icons.check_circle,
-                      iconColor: Colors.tealAccent,
-                    ),
-                    SizedBox(height: 10),
-                    SectionTitle(title: 'This week'),
-                    NotificationCard(
-                      time: '9:01am',
-                      title: 'Alert',
-                      subtitle: 'You have booked a Space at 9:10 am',
-                      icon: Icons.warning,
-                      iconColor: Colors.yellow,
-                    ),
-                  ],
-                ),
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SectionTitle(title: sectionTitleToday),
+                  NotificationCard(
+                    time: notificationTime1,
+                    title: notificationTitle1,
+                    subtitle: notificationSubtitle1,
+                    icon: Icons.check_circle,
+                    iconColor: Colors.tealAccent,
+                  ),
+                  const SizedBox(height: 10),
+                  SectionTitle(title: sectionTitleYesterday),
+                  NotificationCard(
+                    time: notificationTime1,
+                    title: notificationTitle1,
+                    subtitle: notificationSubtitle2,
+                    icon: Icons.check_circle,
+                    iconColor: Colors.tealAccent,
+                  ),
+                  const SizedBox(height: 10),
+                  SectionTitle(title: sectionTitleThisWeek),
+                  NotificationCard(
+                    time: notificationTime1,
+                    title: notificationTitle2,
+                    subtitle: notificationSubtitle3,
+                    icon: Icons.warning,
+                    iconColor: Colors.yellow,
+                  ),
+                ],
               ),
+            ),
           ],
         ),
       ),
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(
-          canvasColor: const Color(0xFF35344A),),
+          canvasColor: const Color(0xFF35344A),
+        ),
         child: Container(
-        decoration: BoxDecoration(
-          // color: const Color(0xFF2C2C54),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.6),
-              spreadRadius: 1,
-              blurRadius: 8,
-              offset: const Offset(0, -3),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed, 
-          backgroundColor: const Color(0xFF35344A), // To ensure the Container color is visible
-          currentIndex: _selectedIndex,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined, size: 30),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.wallet, size: 30),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history, size: 30),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings_outlined, size: 30),
-              label: '',
-            ),
-          ],
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
+          decoration: BoxDecoration(
+            // color: const Color(0xFF2C2C54),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.6),
+                spreadRadius: 1,
+                blurRadius: 8,
+                offset: const Offset(0, -3),
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: const Color(0xFF35344A), // To ensure the Container color is visible
+            currentIndex: _selectedIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.wallet, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined, size: 30),
+                label: '',
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
 
-              if(_selectedIndex == 0){
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const MainPage(),
-                  ),
-                );
-              } else if(_selectedIndex == 1){
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const PaymentMethodPage(),
-                  ),
-                );
-              } else if(_selectedIndex == 2){
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const ParkingHistoryPage(),
-                  ),
-                );
-              } else if(_selectedIndex == 3){
-              
-              }
-            });
-          },
-          selectedItemColor: Colors.grey,
-          unselectedItemColor: Colors.grey,
-          showUnselectedLabels: false,
-          showSelectedLabels: false,
+                if (_selectedIndex == 0) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const MainPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 1) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const PaymentMethodPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 2) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const ParkingHistoryPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 3) {
+                  // 设置页的操作可以放在这里
+                }
+              });
+            },
+            selectedItemColor: Colors.grey,
+            unselectedItemColor: Colors.grey,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+          ),
         ),
       ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   backgroundColor: const Color(0xFF58C6A9),
-      //   shape: const CircleBorder(),
-      //   child: const Icon(
-      //     Icons.near_me,
-      //     color: Colors.white,
-      //   ), 
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       drawer: const SideMenu(),
     );
   }
