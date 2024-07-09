6155bf698b4b9d06755e60ae049cad9ae3c6522b
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-07-09 17:03:55 +0200

    Splash Screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_parking_system/components/login/login_main.dart';
+import 'package:smart_parking_system/components/splashscreen/splash_screen.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb){
+    await Firebase.initializeApp(options: const FirebaseOptions(apiKey: "AIzaSyAkCaUP5fbIQdkGAe8LJJrmFWlbSgTE2Cs", appId: "1:808791551084:web:6cf351cf1ebb0a5238fc49", messagingSenderId: "808791551084", projectId: "parkme-c2508"));
+  }else{
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -15,7 +26,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginMainPage(),
+      home: const SplashScreen(
+        child: LoginMainPage(),
+      )
     );
   }
 }
