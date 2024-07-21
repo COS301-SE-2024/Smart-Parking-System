4d65c4ce367827d6b8c779f8830ec6e9d003195d
Shiza <u21451631@tuks.co.za>
2024-07-22 00:56:30 +0200

    changes

--- lib/components/bookings/select_zone.dart
+++ lib/components/bookings/select_zone.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:smart_parking_system/components/bookings/select_level.dart';
-// import 'package:smart_parking_system/components/main_page.dart';
 
 class ZoneSelectPage extends StatefulWidget {
   final double price;
@@ -12,6 +11,19 @@ class ZoneSelectPage extends StatefulWidget {
 }
 
 class _ZoneSelectPageState extends State<ZoneSelectPage> {
+  String? selectedZone;
+  Map<String, dynamic> zoneDetails = {
+    'A': {'spaces': 200, 'distance': '3 mins drive'},
+    'B': {'spaces': 150, 'distance': '5 mins drive'},
+    // Add more zones as needed
+  };
+
+  void selectZone(String zone) {
+    setState(() {
+      selectedZone = zone;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,34 +31,23 @@ class _ZoneSelectPageState extends State<ZoneSelectPage> {
       body: Container(
         color: const Color(0xFF2D2F41),
         padding: const EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
+        child: Column(
           children: [
             Container(
               padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 15),
               color: const Color(0xFF2D2F41),
               child: Stack(
                 children: [
-                  Builder(
-                    builder: (BuildContext context) {
-                      return IconButton(
-                        icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 30.0),
-                        onPressed: () {
-                          // Replace this with your main page route
-                          // Navigator.of(context).pushReplacement(
-                          //   MaterialPageRoute(
-                          //     builder: (_) => const MainPage(),
-                          //   ),
-                          // );
-                          Navigator.of(context).pop();
-                        },
-                      );
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 30.0),
+                    onPressed: () {
+                      Navigator.of(context).pop();
                     },
                   ),
                   const Align(
                     alignment: Alignment.center,
                     child: Text(
-                      'Parking Slot (Zone)',
+                      'Parking Zones',
                       style: TextStyle(
                         color: Colors.tealAccent,
                         fontSize: 18,
@@ -58,149 +59,194 @@ class _ZoneSelectPageState extends State<ZoneSelectPage> {
               ),
             ),
             const Text(
-              'Select prefered Zone',
+              'Choose Your Parking\nZone',
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 20,
+                fontSize: 28,
                 fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildZoneCard(
-                  context: context,
-                  zone: 'A',
-                  entrance: 'PNP Entrance',
-                  availableSlots: 2,
-                  isAvailable: true,
-                ),
-                _buildZoneCard(
-                  context: context,
-                  zone: 'B',
-                  entrance: 'Woolworths Entrance',
-                  availableSlots: 2,
-                  isAvailable: true,
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _buildZoneCard(
-                  context: context,
-                  zone: 'C',
-                  entrance: 'Clicks Entrance',
-                  availableSlots: 1,
-                  isAvailable: true,
-                ),
-                _buildZoneCard(
-                  context: context,
-                  zone: 'D',
-                  entrance: 'Food Court Entrance',
-                  availableSlots: 0,
-                  isAvailable: false,
-                ),
-              ],
-            ),
-          ],
-        ),
-        ),
-        
-        
-      ),
-    );
-  }
-
-  Widget _buildZoneCard({
-    required BuildContext context,
-    required String zone,
-    required String entrance,
-    required int availableSlots,
-    required bool isAvailable,
-  }) {
-    return GestureDetector(
-      onTap: isAvailable
-          ? () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LevelSelectPage(bookedAddress: widget.bookedAddress, price: widget.price, selectedZone: zone),
-                ),
-              );
-            }
-          : () {
-          showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              backgroundColor: const Color(0xFF2D2F41),
-              title: const Center(
-                child: Text('No Slots Available!', style: TextStyle(color: Colors.white)),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(color: Colors.white),
               ),
-              actions: [
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('OK', style: TextStyle(color: Color(0xFF58C6A9))),
+              child: const Text(
+                '500 spaces available',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: selectedZone == null ? Colors.green : Color(0xFF58C6A9),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Center(
+                    child: Icon(Icons.local_parking, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  'Denotes a Parking Zone',
+                  style: TextStyle(
+                    color: Colors.tealAccent,
+                    fontSize: 18,
                   ),
                 ),
               ],
-            );
-          },
-        );
-      },
-      child: Container(
-        width: 200,
-        height: 200,
-        decoration: BoxDecoration(
-          color: isAvailable ? const Color(0xFF2A4037) : const Color(0xFF490517),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              Text(
-                zone,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
               ),
-              Text(
-                'Zone $zone',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.center,
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Image.asset(
+                        'assets/s-map.png', // Update this with your image path
+                        height: 200,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                      Positioned(
+                        left: 100,
+                        top: 50,
+                        child: GestureDetector(
+                          onTap: () {
+                            selectZone('A');
+                          },
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: selectedZone == 'A' ? Color(0xFF58C6A9) : Colors.green,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Center(
+                              child: Icon(Icons.local_parking, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 200,
+                        top: 100,
+                        child: GestureDetector(
+                          onTap: () {
+                            selectZone('B');
+                          },
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: selectedZone == 'B' ? Color(0xFF58C6A9) : Colors.green,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Center(
+                              child: Icon(Icons.local_parking, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Add more Positioned widgets for other 'P' markers
+                    ],
+                  ),
+                  if (selectedZone != null && zoneDetails.containsKey(selectedZone)) ...[
+                    const SizedBox(height: 20),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF34354A),
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Zone $selectedZone',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            'Spaces Available: ${zoneDetails[selectedZone!]['spaces']} slots',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            'Distance to Zone: ${zoneDetails[selectedZone!]['distance']}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ],
               ),
-              Text(
-                'Available Slots: $availableSlots',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+            ),
+            const Spacer(),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                textAlign: TextAlign.center,
               ),
-              Text(
-                '($entrance)',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.center,
+              onPressed: selectedZone != null ? () {
+                // Handle continue action
+              } : null,
+              child: const Text(
+                'Continue',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_parking_system/components/login/login_main.dart';
-import 'package:smart_parking_system/components/splashscreen/splash_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyAkCaUP5fbIQdkGAe8LJJrmFWlbSgTE2Cs",
-        appId: "1:808791551084:web:6cf351cf1ebb0a5238fc49",
-        messagingSenderId: "808791551084",
-        projectId: "parkme-c2508",
-      ),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -30,18 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Signup & Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(
-        child: LoginMainPage(),
-      ),
+      home: const LoginMainPage(),
     );
   }
 }
-
 
 // import 'package:flutter/material.dart';
 // import 'package:smart_parking_system/components/main_page.dart'; // Import your main_page.dart file here
