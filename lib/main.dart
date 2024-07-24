75229006c6df294f83a4c23d1bce016da1461916
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-07-24 12:22:59 +0200

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:smart_parking_system/components/login/login_main.dart';
+import 'package:smart_parking_system/components/splashscreen/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: dotenv.env['API_KEY']!,
+        appId: "1:808791551084:web:6cf351cf1ebb0a5238fc49",
+        messagingSenderId: "808791551084",
+        projectId: "parkme-c2508",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -11,11 +31,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Signup & Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginMainPage(),
+      home: const SplashScreen(
+        child: LoginMainPage(),
+      ),
     );
   }
 }
