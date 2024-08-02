8baddf0aa0e2eae2d404059f0bfcc9a6af3f09c9
Shiza <u21451631@tuks.co.za>
2024-08-02 10:26:28 +0200

    "about-us, privacy policy and user profile updated"

---
+++ lib/components/settings/aboutUs.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:smart_parking_system/components/main_page.dart';
+import 'package:smart_parking_system/components/parking/parking_history.dart';
+import 'package:smart_parking_system/components/payment/payment_options.dart';
+import 'package:smart_parking_system/components/settings/settings.dart';
+
+class AboutUsPage extends StatefulWidget {
+  const AboutUsPage({Key? key}) : super(key: key);
+
+  @override
+  _AboutUsPageState createState() => _AboutUsPageState();
+}
+
+class _AboutUsPageState extends State<AboutUsPage> {
+  int _selectedIndex = 3; // Set to 3 to highlight the Settings icon
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF2D2F3E),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          backgroundColor: Color(0xFF2D2F3E),
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          flexibleSpace: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                      Text(
+                        'About us',
+                        style: TextStyle(
+                          color: const Color(0xFF58C6A9),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 48), // To balance the layout
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Positioned(
+                        left: -50,
+                        child: Image.asset(
+                          'assets/brush1.png',
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                      Positioned(
+                        right: -50,
+                        child: Image.asset(
+                          'assets/brush2.png',
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                      CustomPaint(
+                        size: Size(300, 300),
+                        painter: CircleWithSplashes(),
+                      ),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(width: 90),
+                              Text(
+                                'OUR',
+                                style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          ClipOval(
+                            child: Image.asset(
+                              'assets/team-photo.png',
+                              width: 200,
+                              height: 200,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'TEAM.',
+                                style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 90),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Text(
+                      'We are a passionate team of developers who thrive on overcoming challenges together. With creativity and bright solutions, we turn every obstacle into an opportunity for innovation.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: const Color(0xFF35344A),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.6),
+                spreadRadius: 1,
+                blurRadius: 8,
+                offset: const Offset(0, -3),
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: const Color(0xFF35344A),
+            currentIndex: _selectedIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.wallet, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history, size: 30),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined, size: 30),
+                label: '',
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+
+                if (_selectedIndex == 0) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const MainPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 1) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const PaymentMethodPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 2) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const ParkingHistoryPage(),
+                    ),
+                  );
+                } else if (_selectedIndex == 3) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsPage(),
+                    ),
+                  );
+                }
+              });
+            },
+            selectedItemColor: const Color(0xFF58C6A9),
+            unselectedItemColor: Colors.grey,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFF58C6A9),
+        shape: const CircleBorder(),
+        child: const Icon(
+          Icons.near_me,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class CircleWithSplashes extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Color(0xFF23223A)
+      ..style = PaintingStyle.fill;
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
