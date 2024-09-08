22215e651a84decbe78b0329a8840a01025e3a64
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-09-08 15:11:08 +0200

    Added strings.xml

--- replace_api_key.dart
+++ replace_api_key.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 void main() {
   // Get the API key from environment variables
   final apiKey = Platform.environment['API_KEY'];
+  final mapsapiKey = Platform.environment['MAPS_API_KEY'];
 
   if (apiKey == null) {
     print('API_KEY environment variable is not set.');
@@ -13,23 +14,39 @@ void main() {
   }
 
   // Path to the google-services.json file
-  const filePath = 'android/app/google-services.json';
+  const googleServicesFilePath = 'android/app/google-services.json';
 
   // Read the google-services.json file
-  final file = File(filePath);
-  final jsonString = file.readAsStringSync();
+  final googleServicesFile = File(googleServicesFilePath);
+  final googleServicesJsonString = googleServicesFile.readAsStringSync();
 
   // Parse the JSON data
-  final config = jsonDecode(jsonString);
+  final googleServicesConfig = jsonDecode(googleServicesJsonString);
 
   // Replace the API key with the environment variable
-  config['client'][0]['api_key'][0]['current_key'] = apiKey;
+  googleServicesConfig['client'][0]['api_key'][0]['current_key'] = apiKey;
 
   // Convert the modified JSON back to a string
-  final updatedConfig = jsonEncode(config);
+  final updatedGoogleServicesConfig = jsonEncode(googleServicesConfig);
 
   // Write the updated configuration back to the file
-  file.writeAsStringSync(updatedConfig);
+  googleServicesFile.writeAsStringSync(updatedGoogleServicesConfig);
 
-  print('API key replaced successfully.');
+  print('API key replaced successfully in google-services.json.');
+
+  // Path to the strings.xml file
+  const stringsFilePath = 'android/app/src/main/res/values/strings.xml';
+
+  // Read the strings.xml file
+  final stringsFile = File(stringsFilePath);
+  final stringsXmlString = stringsFile.readAsStringSync();
+
+  // Replace the placeholders with the environment variable
+  final updatedStringsXmlString = stringsXmlString
+      .replaceAll('<string name="MAPS_API_KEY"></string>', '<string name="MAPS_API_KEY">$mapsapiKey</string>');
+
+  // Write the updated strings.xml back to the file
+  stringsFile.writeAsStringSync(updatedStringsXmlString);
+
+  print('API keys replaced successfully in strings.xml.');
 }
