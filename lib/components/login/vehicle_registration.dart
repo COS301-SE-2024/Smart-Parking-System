dcdade39988863cf6770aa866be0679d3c02fd04
Mekhail Muller <64637963+MekhailMuller@users.noreply.github.com>
2024-09-12 17:24:26 +0200

    Made Vehicle Registration at Signup consistent

--- lib/components/login/vehicle_registration.dart
+++ lib/components/login/vehicle_registration.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:smart_parking_system/components/common/custom_widgets.dart';
 import 'package:smart_parking_system/components/common/toast.dart';
 import 'package:smart_parking_system/components/login/registration_successful.dart';
 
@@ -18,20 +16,12 @@ class _CarRegistrationState extends State<CarRegistration> {
   final TextEditingController _modelController = TextEditingController();
   final TextEditingController _colorController = TextEditingController();
   final TextEditingController _licenseController = TextEditingController();
+  bool isLoading = false;
 
   Future<void> validateVehicleDetails() async {
     // Validate Vehicle details
-    bool bValid = true;
-
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(
-        builder: (context) => const SuccessionPage(),
-      ),
-    );
-
-    if (bValid) {
-      _register();
-    }
+    await _register();   
+    // showToast(message: 'Please fill in all fields');
   }
     
 
@@ -40,7 +30,9 @@ class _CarRegistrationState extends State<CarRegistration> {
     final String model = _modelController.text;
     final String color = _colorController.text;
     final String license = _licenseController.text;
-
+    setState((){
+      isLoading = true;
+    });
     try {
       User? user = FirebaseAuth.instance.currentUser;
 
@@ -55,13 +47,22 @@ class _CarRegistrationState extends State<CarRegistration> {
 
         showToast(message: 'Vehicle Added Successfully!');
         // ignore: use_build_context_synchronously
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const SuccessionPage(),
-          ),
-        );
+        setState((){
+          isLoading = false;
+        });
+        if(mounted) { // Check if the widget is still in the tree
+             Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const SuccessionPage(),
+            ),
+          );
+        }
+       
       }
     } catch (e) {
+      setState((){
+        isLoading = false;
+      });
       showToast(message: 'Error: $e');
     }
   }
@@ -69,222 +70,173 @@ class _CarRegistrationState extends State<CarRegistration> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          // Background image
-          SvgPicture.asset(
-            'assets/Background - Small.svg',
-            fit: BoxFit.fill,
-          ),
-          // Foreground elements
-          SingleChildScrollView(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
-            child: 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              // Logo above the white container
-              Image.asset(
-                'assets/car_temp.png',
-                height: 200, // Adjust the height as needed
-                width: 200,  // Adjust the width as needed
-              ),
-              const SizedBox(height: 20), // Space between logo and container
-              // Container for login form
-              Container(
-                height: MediaQuery.of(context).size.height,
-                width: 500,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    topRight: Radius.circular(25),
-                  ),
-                ),
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const SizedBox(height: 30),  // Space before the "Add Your Vehicle" text
-                    const Text(
+      backgroundColor: const Color(0xFF35344A),
+      body: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            width: 550,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 40.0), // Space from the top
+                  const Center(
+                    child: Text(
                       'Add Your Vehicle',
                       style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
                         color: Color(0xFF58C6A9),
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    const SizedBox(height: 25), // Space between the "Add Vehicle" text and text boxes
-                    TextField(
-                      controller: _brandController,
-                      decoration: InputDecoration(
-                        labelText: 'Vehicle Brand',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Darker grey for label text
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Color for floating label when focused
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xFFD9D9D9), // Light grey background color
-                        contentPadding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 20.0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when enabled
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when focused
-                          ),
-                        ),
+                  ),
+                
+                  const SizedBox(height: 60.0),
+                  TextField(
+                    controller: _brandController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      labelText: 'Vehicle Brand',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
-                      style: TextStyle(
-                        color: Colors.grey.shade800, // Dark grey input text color
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
                     ),
-                    const SizedBox(height: 20,),
-                    TextField(
-                      controller: _modelController,
-                      decoration: InputDecoration(
-                        labelText: 'Vehicle Model',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Darker grey for label text
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Color for floating label when focused
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xFFD9D9D9), // Light grey background color
-                        contentPadding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 20.0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when enabled
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when focused
-                          ),
-                        ),
+                    keyboardType: TextInputType.number,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 10.0),
+                  TextField(
+                    controller: _modelController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      labelText: 'Vehicle Model',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
-                      style: TextStyle(
-                        color: Colors.grey.shade800, // Dark grey input text color
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller: _colorController,
-                      decoration: InputDecoration(
-                        labelText: 'Color',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Darker grey for label text
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Color for floating label when focused
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xFFD9D9D9), // Light grey background color
-                        contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when enabled
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when focused
-                          ),
-                        ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  TextField(
+                    controller: _colorController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      labelText: 'Color',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
-                      style: TextStyle(
-                        color: Colors.grey.shade800, // Dark grey input text color
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      controller: _licenseController,
-                      decoration: InputDecoration(
-                        labelText: 'License Number',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Darker grey for label text
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade700, // Color for floating label when focused
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xFFD9D9D9), // Light grey background color
-                        contentPadding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 20.0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when enabled
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFD9D9D9), // Border color when focused
-                          ),
-                        ),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 10.0),
+                  TextField(
+                    controller: _licenseController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.transparent,
+                      labelText: 'License Number',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
-                      style: TextStyle(
-                        color: Colors.grey.shade800, // Dark grey input text color
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
                       ),
                     ),
-                    const SizedBox(height: 40),
-                    nextButtonWithSkip(
-                      displayText: 'Continue',
-                      action: validateVehicleDetails,
-                      nextPage: const SuccessionPage(),
-                      context: context
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 60.0),
+                  // nextButtonWithSkip(
+                  //   displayText: 'Continue',
+                  //   action: validateVehicleDetails,
+                  //   nextPage: const CarRegistration(),
+                  //   context: context
+                  // ),
+                  ElevatedButton(
+                      onPressed: () {
+                        validateVehicleDetails();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 100,
+                          vertical: 12,
+                        ),
+                        backgroundColor: const Color(0xFF58C6A9),
+                      ),
+                      child: isLoading
+                        ? const SizedBox(
+                            width: 24,
+                            height: 24,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 2.0,
+                            ),
+                          )
+                        : const Text(
+                        'Continue',
+                        style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w400),
+                      ),
                     ),
-                  ],
-                ),
+                  const SizedBox(height: 20.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (_) => const SuccessionPage(),
+                        ),
+                      );
+                    },
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Color.fromARGB(255, 199, 199, 199),
+                            thickness: 1,
+                            endIndent: 10,
+                          ),
+                        ),
+                        Text(
+                          'Skip for now',
+                          style: TextStyle(
+                            color: Color(0xFF58C6A9),
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Color.fromARGB(255, 199, 199, 199),
+                            thickness: 1,
+                            indent: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-          ),
-        ],
+        ),
       ),
     );
   }
