2544cf79d538bf7342d0497dd453efee1b3bd530
Mekhail Muller <64637963+MekhailMuller@users.noreply.github.com>
2024-09-17 21:51:56 +0200

    Added total of 19 car brands

--- lib/components/bookings/select_vehicle.dart
+++ lib/components/bookings/select_vehicle.dart
@@ -64,17 +64,53 @@ class _ViewVehiclePageState extends State<ChooseVehiclePage> {
         // Add the imageDirector based on the vehicleBrand
         String imageDirector;
         switch (data['vehicleBrand']?.toLowerCase()) {
-          case 'vw':
-            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FVW_Logo.png?alt=media&token=f3dcb770-eafe-44d4-ba83-12dcffd93094';
-            break;
           case 'audi':
             imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FAudi_Logo.png?alt=media&token=ae374933-220d-46a9-b3bb-7d92334eb0a9';
             break;
           case 'bmw':
             imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FBMW_Logo.png?alt=media&token=26622a31-0ed6-44fd-9c05-b30ae5b34f2d';
             break;
+          case 'chevrolet':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FChevrolet_Logo.png?alt=media&token=00365703-146d-4daa-b839-c9cdf2f68b43';
+            break;
           case 'ford':
             imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FFord_Logo.png?alt=media&token=2a544019-af34-4871-b4fa-db433f662f55';
+            break;
+          case 'haval':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FHaval_Logo.png?alt=media&token=283a1adb-1407-4925-b8c6-cc290eb85ad8';
+            break;
+          case 'honda':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FHonda_Logo.png?alt=media&token=bb6e1fdf-cad3-44b1-affd-20b0f85d7fb8';
+            break;
+          case 'hyundai':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FHyundai_Logo.png?alt=media&token=30e724ae-52fa-475e-b424-0ed7f7f3323b';
+            break;
+          case 'isuzu':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FIsuzu_Logo.png?alt=media&token=c7f7eff8-0f72-4e4f-9154-983661dd33d3';
+            break;
+          case 'kia':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FKia_Logo.png?alt=media&token=7e6f4ead-4b43-498d-a9a2-8ceb62503da0';
+            break;
+          case 'mahindra':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FMahindra_Logo.png?alt=media&token=5c930729-e4ac-43b6-8bc6-bf43d0daf194';
+            break;
+          case 'mazda':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FMazda_Logo.png?alt=media&token=61b5dc0a-5956-4c7d-917a-4be462ce35fd';
+            break;
+          case 'mercedes':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FMercedes_Benz_Logo.png?alt=media&token=a0970710-44df-451f-9273-23d9d586bf74';
+            break;
+          case 'mercedes-benz':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FMercedes_Benz_Logo.png?alt=media&token=a0970710-44df-451f-9273-23d9d586bf74';
+            break;
+          case 'nissan':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FNissan_Logo.png?alt=media&token=32347ed8-58e0-495b-a084-b35f6a89ec69';
+            break;
+          case 'opel':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FOpel_Logo.png?alt=media&token=098ab2d9-eab6-478c-a19a-24099addcd69';
+            break;
+          case 'renault':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FRenault_Logo.png?alt=media&token=2b3821b5-4ae3-4ca6-ad70-2c30efcb88f5';
             break;
           case 'suzuki':
             imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FSuzuki_Logo.png?alt=media&token=43e5167d-c51f-4345-9479-74ec04c57908';
@@ -82,8 +118,11 @@ class _ViewVehiclePageState extends State<ChooseVehiclePage> {
           case 'toyota':
             imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FSuzuki_Logo.png?alt=media&token=43e5167d-c51f-4345-9479-74ec04c57908';
             break;
-          case 'mazda':
-            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FMazda_Logo.png?alt=media&token=61b5dc0a-5956-4c7d-917a-4be462ce35fd';
+          case 'volvo':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FVolvo_Logo.png?alt=media&token=6eb02bdb-2102-47d6-bedc-e28bc0993282';
+            break;
+          case 'vw':
+            imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2FVW_Logo.png?alt=media&token=f3dcb770-eafe-44d4-ba83-12dcffd93094';
             break;
           default:
             imageDirector = 'https://firebasestorage.googleapis.com/v0/b/parkme-c2508.appspot.com/o/vehiclelogo%2Fdefault_logo.png?alt=media&token=735c5077-5c11-498c-9d24-5481ba932e99'; // You might want to have a default logo
