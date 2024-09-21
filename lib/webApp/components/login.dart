0936aa5232763b55f1257710d90555bd38a7209d
Shiza <u21451631@tuks.co.za>
2024-09-21 15:50:59 +0200

    login fix

--- lib/webApp/components/login.dart
+++ lib/webApp/components/login.dart
@@ -246,7 +246,7 @@ class _LoginPageState extends State<LoginPage> {
                 Expanded(
                   flex: 1,
                   child: Image.asset(
-                    'assets/parking.png', // Replace with your images
+                    'assets/parking.png', //image path
                     fit: BoxFit.cover,
                   ),
                 ),
