35a30aa6676094d4420b211c3c51b4fa01e1f2b1
Mr.Who <hjh19990814@gmail.com>
2024-09-21 20:05:32 +0200

    fix UI issue

--- lib/WebComponents/profilePage/profile_card.dart
+++ lib/WebComponents/profilePage/profile_card.dart
@@ -8,7 +8,7 @@ class ProfileCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: const Color(0xFF23223A),
+        color: const Color(0xFF1A1F37),
         borderRadius: BorderRadius.circular(14),
       ),
       child: const Column(
