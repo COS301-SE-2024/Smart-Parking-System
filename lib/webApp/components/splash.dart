059cc834a01002a13bb46e0d6f34c4752bc6ab2e
Shiza <u21451631@tuks.co.za>
2024-09-22 15:25:04 +0200

    updated splash

--- lib/webApp/components/splash.dart
+++ lib/webApp/components/splash.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:smart_parking_system/webapp/components/login.dart';
+import 'package:smart_parking_system/webapp/components/registration1.dart';
 
 class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
@@ -17,161 +19,177 @@ class Splash extends StatelessWidget {
           displayColor: Colors.white,
         ),
       ),
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/backW.png"),
-                  fit: BoxFit.cover,
-                ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginPage(),
+        '/registration': (context) => RegistrationPage()
+      },
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/backW.png"),
+                fit: BoxFit.cover,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Row(
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            // Moved logo to the left
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10), // Adjust this value to move logo
-                              child: Center(
-                                child: Image.asset(
-                                  'assets/logo1.png',
-                                  height: constraints.maxHeight * 0.25,
-                                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // Moved logo to the left
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Center(
+                              child: Image.asset(
+                                'assets/logo1.png',
+                                height: constraints.maxHeight * 0.25,
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 60),
-                              child: Text(
-                                'Never waste time\nsearching for\nparking again!',
-                                style: TextStyle(
-                                  fontSize: constraints.maxWidth * 0.06,
-                                  fontWeight: FontWeight.w900,
-                                  color: Colors.white,
-                                  height: 1.0,
-                                ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 60),
+                            child: Text(
+                              'Never waste time\nsearching for\nparking again!',
+                              style: TextStyle(
+                                fontSize: constraints.maxWidth * 0.06,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.white,
+                                height: 1.0,
                               ),
                             ),
-                            const SizedBox(height: 40),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 60, top: 50, bottom: 30),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: constraints.maxWidth * 0.15,
-                                    child: ElevatedButton(
-                                      onPressed: () {},
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Color(0xFF58C6A9),
-                                        padding: const EdgeInsets.symmetric(vertical: 15),
-                                      ),
-                                      child: Text(
-                                        'Log in',
-                                        style: TextStyle(
-                                          fontSize: constraints.maxWidth * 0.015,
-                                          fontWeight: FontWeight.w900,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 20),
-                                  SizedBox(
-                                    width: constraints.maxWidth * 0.15,
-                                    child: ElevatedButton(
-                                      onPressed: () {},
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.blueGrey,
-                                        padding: const EdgeInsets.symmetric(vertical: 15),
-                                      ),
-                                      child: Text(
-                                        'Client Register',
-                                        style: TextStyle(
-                                          fontSize: constraints.maxWidth * 0.015,
-                                          fontWeight: FontWeight.w900,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const SizedBox(height: 40),
-                            // Centered "Or Sign up with" section
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                          ),
+                          const SizedBox(height: 40),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 60, top: 50, bottom: 30),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Image.asset(
-                                  'assets/line.png', // Replace with your left image
-                                  width: 200, // Adjust width as needed
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                                  child: Text(
-                                    'Or Sign up with',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: constraints.maxWidth * 0.012,
-                                      fontWeight: FontWeight.w900,
+                                SizedBox(
+                                  width: constraints.maxWidth * 0.15,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/login');
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Color(0xFF58C6A9),
+                                      padding: const EdgeInsets.symmetric(vertical: 15),
+                                    ),
+                                    child: Text(
+                                      'Log in',
+                                      style: TextStyle(
+                                        fontSize: constraints.maxWidth * 0.015,
+                                        fontWeight: FontWeight.w900,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
                                 ),
-                                Image.asset(
-                                  'assets/line.png', // Replace with your right image
-                                  width: 200, // Adjust width as needed
+                                const SizedBox(width: 20),
+                                SizedBox(
+                                  width: constraints.maxWidth * 0.15,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/registration');
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blueGrey,
+                                      padding: const EdgeInsets.symmetric(vertical: 15),
+                                    ),
+                                    child: Text(
+                                      'Client Register',
+                                      style: TextStyle(
+                                        fontSize: constraints.maxWidth * 0.015,
+                                        fontWeight: FontWeight.w900,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ],
                             ),
-                            const SizedBox(height: 10),
-                            Center(
-                              child: Image.asset(
-                                'assets/google-play.png',
-                                height: constraints.maxHeight * 0.06,
+                          ),
+                          const SizedBox(height: 40),
+                          // Centered "Or Sign up with" section
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/line.png',
+                                width: 200,
                               ),
-                            ),
-                            const SizedBox(height: 20),
-                            Center(
-                              child: Text(
-                                '© 2024 DaVinci Code. All rights reserved.\nPrivacy policy | Contact us',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: constraints.maxWidth * 0.01,
-                                  fontWeight: FontWeight.w900,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 10),
+                                child: Text(
+                                  'Or Sign up with',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: constraints.maxWidth * 0.012,
+                                    fontWeight: FontWeight.w900,
+                                  ),
                                 ),
-                                textAlign: TextAlign.center,
                               ),
+                              Image.asset(
+                                'assets/line.png',
+                                width: 200,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Center(
+                            child: Image.asset(
+                              'assets/google-play.png',
+                              height: constraints.maxHeight * 0.06,
                             ),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(height: 20),
+                          Center(
+                            child: Text(
+                              '© 2024 DaVinci Code. All rights reserved.\nPrivacy policy | Contact us',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: constraints.maxWidth * 0.01,
+                                fontWeight: FontWeight.w900,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
                       ),
-                      Expanded(
-                        flex: 2,
-                        child: Image.asset(
-                          'assets/parking.png',
-                          fit: BoxFit.contain,
-                        ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Image.asset(
+                        'assets/parking.png',
+                        fit: BoxFit.contain,
                       ),
-                    ],
-                  );
-                },
-              ),
+                    ),
+                  ],
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
