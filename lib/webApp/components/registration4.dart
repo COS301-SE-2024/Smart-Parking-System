f494e4fc25e289f11a6474113067734e2431df54
Shiza <u21451631@tuks.co.za>
2024-09-22 20:38:23 +0200

    lint

--- lib/webApp/components/registration4.dart
+++ lib/webApp/components/registration4.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Registration4 extends StatefulWidget {
-  const Registration4({Key? key}) : super(key: key);
+  const Registration4({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _Registration4State createState() => _Registration4State();
 }
 
