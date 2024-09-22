2725ab2aeeed87bd296ec8d28c45322175c64db9
Shiza <u21451631@tuks.co.za>
2024-09-22 17:34:22 +0200

    reg3

--- lib/webApp/components/registration3.dart
+++ lib/webApp/components/registration3.dart
@@ -40,97 +40,102 @@ class _Registration3State extends State<Registration3> {
                           ),
                           color: const Color(0xFF23223A),
                           elevation: 6.0,
-                          child: SingleChildScrollView(
-                            padding: const EdgeInsets.all(30.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Center(
-                                  child: Image.asset(
-                                    'assets/logo2.png',
-                                    height: 70,
-                                  ),
-                                ),
-                                const SizedBox(height: 20),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    _buildStyledStepIndicator(1, false),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(2, false),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(3, true), // Current step
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(4, false),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(5, false),
-                                  ],
-                                ),
-                                const SizedBox(height: 40),
-                                
-                                _buildLabeledTextField('Total parking slots *', 'Enter total parking slots'),
-                                const SizedBox(height: 15),
-                                _buildLabeledTextField('Number of zones *', 'Enter number of zones'),
-                                const SizedBox(height: 15),
-                                _buildLabeledTextField('Number of slots in each zone *', 'Enter number of slots per zone'),
-                                const SizedBox(height: 15),
-                                _buildLabeledTextField('Number of floors *', 'Enter number of floors'),
-                                const SizedBox(height: 15),
-                                _buildLabeledTextField('Number of slots on each floor *', 'Enter slots per floor'),
-                                const SizedBox(height: 15),
-                                _buildLabeledTextField('Number of rows *', 'Enter number of rows'),
-
-                                const SizedBox(height: 25),
-                                Center(
-                                  child: SizedBox(
-                                    width: 200,
-                                    height: 40,
-                                    child: ElevatedButton(
-                                      onPressed: () {
-                                        // Handle next step action
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: const Color(0xFF58C6A9),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(20),
+                          child: SizedBox(
+                            height: double.infinity, // Make the card fill the entire height
+                            child: Padding(
+                              padding: const EdgeInsets.all(30.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Center(
+                                        child: Image.asset(
+                                          'assets/logo2.png',
+                                          height: 70,
                                         ),
                                       ),
-                                      child: const Text(
-                                        'Next',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                        ),
+                                      const SizedBox(height: 20),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          _buildStyledStepIndicator(1, false),
+                                          const SizedBox(width: 10),
+                                          _buildStyledStepIndicator(2, false),
+                                          const SizedBox(width: 10),
+                                          _buildStyledStepIndicator(3, true), // Current step
+                                          const SizedBox(width: 10),
+                                          _buildStyledStepIndicator(4, false),
+                                          const SizedBox(width: 10),
+                                          _buildStyledStepIndicator(5, false),
+                                        ],
                                       ),
-                                    ),
+                                      const SizedBox(height: 40),
+                                      _buildLabeledTextField('Number of zones *', 'Enter number of zones'),
+                                      const SizedBox(height: 15),
+                                      _buildLabeledTextField('Number of floors in each zone *', 'Enter number of floors'),
+                                      const SizedBox(height: 15),
+                                      _buildLabeledTextField('Number of rows on each floor *', 'Enter number of rows'),
+                                      const SizedBox(height: 15),
+                                      _buildLabeledTextField('Number of slots in each row  *', 'Enter number of slots'),
+                                    ],
                                   ),
-                                ),
-                                const SizedBox(height: 15),
-                                Center(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      text: "Need to change details? ",
-                                      style: const TextStyle(color: Colors.white70, fontSize: 12),
-                                      children: [
-                                        TextSpan(
-                                          text: 'Go back',
-                                          style: const TextStyle(
-                                            color: Color(0xFF58C6A9),
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                          ),
-                                          recognizer: TapGestureRecognizer()
-                                            ..onTap = () {
-                                              // Navigate to the previous step
+                                  Column(
+                                    children: [
+                                      Center(
+                                        child: SizedBox(
+                                          width: 200,
+                                          height: 40,
+                                          child: ElevatedButton(
+                                            onPressed: () {
+                                              // Handle next step action
                                             },
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: const Color(0xFF58C6A9),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(20),
+                                              ),
+                                            ),
+                                            child: const Text(
+                                              'Next',
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ),
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                      const SizedBox(height: 15),
+                                      Center(
+                                        child: RichText(
+                                          text: TextSpan(
+                                            text: "Already have an account?",
+                                            style: const TextStyle(color: Colors.white70, fontSize: 12),
+                                            children: [
+                                              TextSpan(
+                                                text: 'Log in',
+                                                style: const TextStyle(
+                                                  color: Color(0xFF58C6A9),
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                                recognizer: TapGestureRecognizer()
+                                                  ..onTap = () {
+                                                    // Navigate to the previous step
+                                                  },
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         ),
@@ -163,7 +168,7 @@ class _Registration3State extends State<Registration3> {
         child: Center(
           child: Text(
             step.toString(),
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
               fontSize: 16,
@@ -180,7 +185,7 @@ class _Registration3State extends State<Registration3> {
       children: [
         Text(
           label,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.bold,
@@ -191,6 +196,7 @@ class _Registration3State extends State<Registration3> {
       ],
     );
   }
+
   Widget _buildTextField(String hintText, {bool obscureText = false}) {
     return TextField(
       obscureText: obscureText,
@@ -205,11 +211,12 @@ class _Registration3State extends State<Registration3> {
         focusedBorder: const UnderlineInputBorder(
           borderSide: BorderSide(color: Color(0xFF58C6A9)),
         ),
-        contentPadding: EdgeInsets.symmetric(vertical: 8),
+        contentPadding: const EdgeInsets.symmetric(vertical: 8),
       ),
     );
   }
 }
+
 class ArrowClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
