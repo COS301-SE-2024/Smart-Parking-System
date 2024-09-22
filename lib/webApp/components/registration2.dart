842255524aa53a87db85c3fb7515c7cbe03caaf8
Shiza <u21451631@tuks.co.za>
2024-09-22 19:27:46 +0200

    reg2

--- lib/webApp/components/registration2.dart
+++ lib/webApp/components/registration2.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
+
+import 'package:smart_parking_system/webapp/components/registration3.dart';
+import 'package:smart_parking_system/webapp/components/registration4.dart';
+import 'package:smart_parking_system/webapp/components/registration5.dart';
 
 class Registration2 extends StatefulWidget {
   const Registration2({Key? key}) : super(key: key);
@@ -21,155 +24,44 @@ class _Registration2State extends State<Registration2> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Positioned.fill(
-            child: Image.asset(
-              'assets/backW.png',
-              fit: BoxFit.cover,
-            ),
-          ),
+          const SizedBox(height: 40),
+          _buildLabeledTextField('Parking location *', 'Enter the parking address'),
+          const SizedBox(height: 15),
+          _buildOperationalHoursField('Operational hours *'),
+          const SizedBox(height: 25),
           Center(
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: EdgeInsets.zero,
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Container(
-                        width: 600,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(70),
-                              bottomRight: Radius.circular(70),
-                            ),
-                          ),
-                          color: const Color(0xFF23223A),
-                          elevation: 6.0,
-                          child: SingleChildScrollView(
-                            padding: const EdgeInsets.all(30.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Center(
-                                  child: Image.asset(
-                                    'assets/logo2.png',
-                                    height: 70,
-                                  ),
-                                ),
-                                const SizedBox(height: 20),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    _buildStyledStepIndicator(1, false),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(2, true),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(3, false),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(4, false),
-                                    const SizedBox(width: 10),
-                                    _buildStyledStepIndicator(5, false),
-                                  ],
-                                ),
-                                const SizedBox(height: 40),
-                                _buildLabeledTextField('Parking location *', 'Enter the parking address'),
-                                const SizedBox(height: 15),
-                                _buildOperationalHoursField('Operational hours *'),
-                                const SizedBox(height: 25),
-                                Center(
-                                  child: SizedBox(
-                                    width: 200,
-                                    height: 40,
-                                    child: ElevatedButton(
-                                      onPressed: () {
-                                        // Handle next step action
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: const Color(0xFF58C6A9),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(20),
-                                        ),
-                                      ),
-                                      child: const Text(
-                                        'Next',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 15),
-                                Center(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      text: "Already have an account? ",
-                                      style: const TextStyle(color: Colors.white70, fontSize: 12),
-                                      children: [
-                                        TextSpan(
-                                          text: 'Log in',
-                                          style: const TextStyle(
-                                            color: Color(0xFF58C6A9),
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                          ),
-                                          recognizer: TapGestureRecognizer()
-                                            ..onTap = () {
-                                              // Navigate to the previous step
-                                            },
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+            child: SizedBox(
+              width: 200,
+              height: 40,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Registration3()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF58C6A9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: Image.asset(
-                    'assets/parking.png',
-                    fit: BoxFit.cover,
+                child: const Text(
+                  'Next',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              ],
+              ),
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildStyledStepIndicator(int step, bool isActive) {
-    return ClipPath(
-      clipper: ArrowClipper(),
-      child: Container(
-        width: 85,
-        height: 80,
-        color: isActive ? const Color(0xFF58C6A9) : const Color(0xFF2B2B45),
-        child: Center(
-          child: Text(
-            step.toString(),
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
-          ),
-        ),
       ),
     );
   }
@@ -180,7 +72,7 @@ class _Registration2State extends State<Registration2> {
       children: [
         Text(
           label,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.bold,
@@ -217,7 +109,7 @@ class _Registration2State extends State<Registration2> {
       children: [
         Text(
           label,
-          style: TextStyle(
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.bold,
@@ -233,7 +125,7 @@ class _Registration2State extends State<Registration2> {
                 children: [
                   Text(
                     entry.key,
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.white,
                       fontSize: 14,
                     ),
@@ -244,7 +136,7 @@ class _Registration2State extends State<Registration2> {
                       children: [
                         Text(
                           entry.value,
-                          style: TextStyle(
+                          style: const TextStyle(
                             color: Colors.white,
                             fontSize: 14,
                           ),
@@ -254,7 +146,7 @@ class _Registration2State extends State<Registration2> {
                           onPressed: () async {
                             await _selectTimeRange(entry.key);
                           },
-                          icon: Icon(Icons.edit, color: Colors.white70),
+                          icon: const Icon(Icons.edit, color: Colors.white70),
                         ),
                       ],
                     ),
@@ -276,7 +168,7 @@ class _Registration2State extends State<Registration2> {
         return Theme(
           data: ThemeData.light().copyWith(
             timePickerTheme: TimePickerThemeData(
-              dialHandColor: Color(0xFF58C6A9),
+              dialHandColor: const Color(0xFF58C6A9),
               hourMinuteColor: Colors.white,
               hourMinuteTextColor: Colors.black,
               dialTextColor: Colors.black,
@@ -287,7 +179,7 @@ class _Registration2State extends State<Registration2> {
               onSurface: Colors.black,
             ),
           ),
-          child: child!, // Return child directly
+          child: child!,
         );
       },
     );
@@ -300,7 +192,7 @@ class _Registration2State extends State<Registration2> {
           return Theme(
             data: ThemeData.light().copyWith(
               timePickerTheme: TimePickerThemeData(
-                dialHandColor: Color(0xFF58C6A9),
+                dialHandColor: const Color(0xFF58C6A9),
                 hourMinuteColor: Colors.white,
                 hourMinuteTextColor: Colors.black,
                 dialTextColor: Colors.black,
@@ -311,7 +203,7 @@ class _Registration2State extends State<Registration2> {
                 onSurface: Colors.black,
               ),
             ),
-            child: child!, // Return child directly
+            child: child!,
           );
         },
       );
@@ -322,25 +214,5 @@ class _Registration2State extends State<Registration2> {
         });
       }
     }
-  }
-}
-
-class ArrowClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.moveTo(0, size.height * 0.25);
-    path.lineTo(size.width * 0.9, size.height * 0.25);
-    path.lineTo(size.width, size.height * 0.5);
-    path.lineTo(size.width * 0.9, size.height * 0.75);
-    path.lineTo(0, size.height * 0.75);
-    path.lineTo(size.width * 0.1, size.height * 0.5);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
