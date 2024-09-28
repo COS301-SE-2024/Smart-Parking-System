a57db6b96478996748aaee952036230cc041316d
Shiza <u21451631@tuks.co.za>
2024-09-28 23:49:19 +0200

    new reg5 page sequence

--- lib/webApp/components/registration5.dart
+++ lib/webApp/components/registration5.dart
@@ -1,11 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_parking_system/components/common/common_functions.dart';
 import 'package:smart_parking_system/components/common/toast.dart';
 
 import 'package:smart_parking_system/webApp/components/registration1.dart';
-
 
 class Registration5 extends StatefulWidget {
   final Function onRegisterComplete;
@@ -19,51 +15,27 @@ class Registration5 extends StatefulWidget {
 }
 
 class _Registration5State extends State<Registration5> {
-  final TextEditingController _billingNameController = TextEditingController();
-  final TextEditingController _accountNumberController = TextEditingController();
-  final TextEditingController _accountTypeController = TextEditingController();
-  final TextEditingController _bankController = TextEditingController();
+  double _pricePerHour = 20; // Default price
   bool _isLoading = false;
 
-  Future<void> _saveCardDetails() async {
+  Future<void> _clientRegisterParkingDetails() async {
     setState((){
       _isLoading = true;
     });
-
-    User? user = FirebaseAuth.instance.currentUser;
-
-    final String billingName = _billingNameController.text;
-    final String accountNumber = _accountNumberController.text.replaceAll(RegExp(r'\s+'), '');
-    final String accountType = _accountTypeController.text;
-    final String bank = _bankController.text;
-
-    if(!isValidString(accountNumber, r'^[0-9]{8,12}$')){showToast(message: "Invalid Account Number. Must be 8-12 digits long."); return;}
-    if(!isValidString(billingName, r'^[a-zA-Z/\s]+$')){showToast(message: "Invalid Holder Name"); return;}
-    if(!isValidString(accountType, r'^[a-zA-Z/\s]+$')){showToast(message: "Invalid Holder Name"); return;}
-    if(!isValidString(bank, r'^[a-zA-Z]+$')){showToast(message: "Invalid Bank Name"); return;}
-
-
-
-    if (user != null) {
-      try {
-        await FirebaseFirestore.instance.collection('client_cards').add({
-          'userId': user.uid,
-          'billingName': billingName,
-          'accountNumber': accountNumber,
-          'accountType': accountType,
-          'bank': bank,
-        });
-        
-        widget.onRegisterComplete();
-      } catch (e) {
-        showToast(message: 'Failed to save card details: $e');
-      }
+    
+    try {
+      widget.ps.price = _pricePerHour.toString();
+      
+      widget.onRegisterComplete();
+    } catch (e) {
+      showToast(message: 'Error: $e');
     }
+
     setState((){
       _isLoading = false;
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -71,13 +43,9 @@ class _Registration5State extends State<Registration5> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 40),
-          _buildLabeledTextField('Enter billing name *', 'Enter name', _billingNameController),
+          _buildPriceText(),
           const SizedBox(height: 15),
-          _buildLabeledTextField('Account number *', 'Enter number', _accountNumberController),
-          const SizedBox(height: 15),
-          _buildLabeledTextField('Account type *', 'Enter type', _accountTypeController),
-          const SizedBox(height: 15),
-          _buildLabeledTextField('Bank *', 'Enter bank name', _bankController),
+          _buildPriceSlider(),
           const SizedBox(height: 25),
           Center(
             child: SizedBox(
@@ -85,8 +53,8 @@ class _Registration5State extends State<Registration5> {
               height: 40,
               child: ElevatedButton(
                 onPressed: () {
-                  // Handle the submission or navigation to the next step
-                  _saveCardDetails();
+                  // Handle next step action
+                  _clientRegisterParkingDetails();
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF58C6A9),
@@ -104,7 +72,7 @@ class _Registration5State extends State<Registration5> {
                     ),
                   )
                 : const Text(
-                  'Join',
+                  'Next',
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
@@ -119,37 +87,44 @@ class _Registration5State extends State<Registration5> {
     );
   }
 
-  Widget _buildLabeledTextField(String label, String hintText, TextEditingController controller, {bool obscureText = false}) {
+  Widget _buildPriceText() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: const TextStyle(
+        const Text(
+          "Select pricing per hour *",
+          style: TextStyle(
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.bold,
           ),
         ),
-        const SizedBox(height: 4),
-        TextField(
-          controller: controller,
-          obscureText: obscureText,
-          style: const TextStyle(color: Colors.white, fontSize: 14),
-          cursorColor: const Color(0xFF58C6A9),
-          decoration: InputDecoration(
-            hintText: hintText,
-            hintStyle: const TextStyle(color: Colors.grey, fontSize: 14),
-            enabledBorder: const UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey),
-            ),
-            focusedBorder: const UnderlineInputBorder(
-              borderSide: BorderSide(color: Color(0xFF58C6A9)),
-            ),
-            contentPadding: const EdgeInsets.symmetric(vertical: 8),
+        const SizedBox(height: 8),
+        Text(
+          "R${_pricePerHour.toInt()}",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
           ),
-        )
+        ),
       ],
+    );
+  }
+
+  Widget _buildPriceSlider() {
+    return Slider(
+      value: _pricePerHour,
+      min: 10,
+      max: 50,
+      divisions: 40,
+      activeColor: const Color(0xFF58C6A9),
+      inactiveColor: Colors.grey,
+      onChanged: (value) {
+        setState(() {
+          _pricePerHour = value;
+        });
+      },
     );
   }
 }
