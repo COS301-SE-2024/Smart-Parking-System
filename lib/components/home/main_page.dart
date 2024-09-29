721282cbc2a98b4b25b1b21578bc30537d44507e
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-09-29 13:08:14 +0200

    EPU-USE Labs object detection code functionality integrated.

--- lib/components/home/main_page.dart
+++ lib/components/home/main_page.dart
@@ -125,6 +125,13 @@ class _MainPageState extends State<MainPage> {
         for (var document in querySnapshot.docs) {
           // Retrieve the fields
           String name = document.get('name') as String;
+
+          // Check if the marker is for "EPI-USE Labs"
+          if (name == 'EPI-USE Labs') {
+            // Call the detectCars function with the appropriate YouTube URL
+            detectCars('https://www.youtube.com/live/CH8GegCF9FI');
+          }
+
           String price = document.get('price') as String;
           String slots = document.get('slots_available') as String;
           double latitude = (document.get('latitude') as num).toDouble();
@@ -160,6 +167,7 @@ class _MainPageState extends State<MainPage> {
                   latitude,
                   longitude,
                 );
+
                 _showParkingInfo();
               },
             ),
@@ -226,7 +234,24 @@ class _MainPageState extends State<MainPage> {
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      
+      int carCount = data['car_count'];
+
+      // Fetch the parking location from Firestore
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      QuerySnapshot querySnapshot = await firestore.collection('parkings').where('name', isEqualTo: 'EPI-USE Labs').get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        // Update the slotsAvailable field for the location
+        DocumentSnapshot document = querySnapshot.docs.first;
+        int availableSlots = 5 - carCount;
+        await firestore.collection('parkings').doc(document.id).update({
+          'slots_available': '$availableSlots/5',
+        });
+
+        showToast(message: 'Updated slots available for EPI-USE Labs to $carCount');
+      } else {
+        showToast(message: 'Parking location not found');
+      }
     } else {
       showToast(message: 'Error: ${response.body}');
     }
