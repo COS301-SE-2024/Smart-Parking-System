ab11df8709959459e80bd955eef3fafcc57f19d9
Ruan Carlinsky <ruancarlinsky01@gmail.com>
2024-09-30 00:22:16 +0200

    Web Parking Zone Marker Functionality Implemented.

--- lib/webApp/components/registration3.dart
+++ lib/webApp/components/registration3.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:smart_parking_system/components/common/toast.dart';
@@ -17,23 +18,75 @@ class Registration3 extends StatefulWidget {
 class _Registration3State extends State<Registration3> {
   bool _isLoading = false;
   final Set<Marker> _markers = {};
+  final Map<MarkerId, String> _markerIdsToFirestoreIds = {};
   late GoogleMapController mapController;
+  late LatLng _center;
+  late BitmapDescriptor _parkingIcon;
+  late String locationName; // Replace with actual user ID
 
-  // You might want to set this to a default location or the user's current location
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  @override
+  void initState() {
+    super.initState();
+    locationName = widget.ps.name; // Initialize userId here
+    _center = LatLng(widget.ps.latitude, widget.ps.longitude);
+    _setCustomMarkerIcon();
+    _addInitialMarker();
+  }
+
+  void _setCustomMarkerIcon() async {
+    _parkingIcon = await BitmapDescriptor.asset(
+      const ImageConfiguration(size: Size(48, 48)),
+      'assets/zone.png', // Ensure you have this asset in your project
+    );
+  }
+
+  void _addInitialMarker() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: const MarkerId('initial_marker'),
+        position: _center,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      ));
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
-  void _addMarker(LatLng position) {
+  void _addMarker(LatLng position) async {
+    final markerId = MarkerId(position.toString());
     setState(() {
       _markers.add(Marker(
-        markerId: MarkerId(position.toString()),
+        markerId: markerId,
         position: position,
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        icon: _parkingIcon,
+        onTap: () => _removeMarker(markerId),
       ));
     });
+
+    // Store marker in Firestore
+    DocumentReference docRef = await FirebaseFirestore.instance.collection('markers').add({
+      'longitude': position.longitude.toStringAsFixed(20),
+      'latitude': position.latitude.toStringAsFixed(20),
+      'location_name': locationName,
+    });
+
+    // Store the Firestore document ID
+    _markerIdsToFirestoreIds[markerId] = docRef.id;
+  }
+
+  void _removeMarker(MarkerId markerId) async {
+    setState(() {
+      _markers.removeWhere((marker) => marker.markerId == markerId);
+    });
+
+    // Remove marker from Firestore
+    if (_markerIdsToFirestoreIds.containsKey(markerId)) {
+      String firestoreId = _markerIdsToFirestoreIds[markerId]!;
+      await FirebaseFirestore.instance.collection('markers').doc(firestoreId).delete();
+      _markerIdsToFirestoreIds.remove(markerId);
+    }
   }
 
   Future<void> _saveParkingDetails() async {
@@ -99,7 +152,7 @@ class _Registration3State extends State<Registration3> {
                   onMapCreated: _onMapCreated,
                   initialCameraPosition: CameraPosition(
                     target: _center,
-                    zoom: 11.0,
+                    zoom: 17.0,
                   ),
                   markers: _markers,
                   onTap: _addMarker,
